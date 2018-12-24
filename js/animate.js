68443449429f44664dbecdda772b3e5dc6654dae
happyanniversaryellie <xreactivity@aol.com>
2018-12-24 09:33:13 -0800

    Added some songs

--- js/animate.js
+++ js/animate.js
@@ -1,1 +1,27 @@
-sd
+const LEN = 27;
+const TIME = 3;
+var slide = 1;
+
+function showSlide(num) {
+    var image = "url('images/" + num + ".jpg')";
+    //document.body.style.backgroundImage = image; 
+    $("body").css("background-image", image);
+}
+
+function runSlideShow() {
+
+    if(slide < LEN) {
+        slide++;
+    } else {
+        slide = 1;
+    }
+
+    showSlide(slide);
+
+    // run slides in a loop
+    setTimeout(runSlideShow, TIME * 1000);
+}
+
+$(document).ready(function(){
+    runSlideShow();
+});
